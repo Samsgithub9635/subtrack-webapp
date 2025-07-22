a996abcb44164b77778629ba3435c2ec8489cb0e
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-07-22 22:36:42 +0530

    Initial commit: Basic Spring Boot project with UI template

---
+++ src/main/java/com/subtrack/subtrack/SubTrackApplication.java
@@ -0,0 +1,13 @@
+package com.subtrack.subtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubTrackApplication.class, args);
+	}
+
+}
