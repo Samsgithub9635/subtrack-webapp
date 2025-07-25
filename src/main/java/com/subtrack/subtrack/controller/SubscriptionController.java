47ba0c198645d0d7f86369dac8230c11f00ca203
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-07-25 23:54:40 +0530

    SubscriptionController class error fixed

--- src/main/java/com/subtrack/subtrack/controller/SubscriptionController.java
+++ src/main/java/com/subtrack/subtrack/controller/SubscriptionController.java
@@ -6,7 +6,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.boot.autoconfigure.pulsar.PulsarProperties.Consumer.Subscription;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,9 +24,9 @@ public class SubscriptionController {
         List<SubscriptionForm> subscriptionList = new ArrayList<>();
 
         // 2. Create some sample subscription objects
-        SubscriptionForm sub1 = new SubscriptionForm("Netflix", LocalDate.now().plusDays(10), 10, ValidityUnit.DAYS, new BigDecimal("15.99"));
-        SubscriptionForm sub2 = new SubscriptionForm("Spotify Premium", LocalDate.now().plusDays(2), 2, ValidityUnit.DAYS, new BigDecimal("9.99"));
-        SubscriptionForm sub3 = new SubscriptionForm("Adobe Creative Cloud", LocalDate.now().plusMonths(2), 2, ValidityUnit.MONTHS, new BigDecimal("52.99"));
+        SubscriptionForm sub1 = new SubscriptionForm(1L, "Netflix", LocalDate.now().plusDays(10), new BigDecimal("15.99"));
+        SubscriptionForm sub2 = new SubscriptionForm(2L, "Spotify Premium", LocalDate.now().plusDays(2), new BigDecimal("9.99"));
+        SubscriptionForm sub3 = new SubscriptionForm(3L, "Adobe Creative Cloud", LocalDate.now().plusMonths(2), new BigDecimal("52.99"));
         
         // 3. Add them to the list
         subscriptionList.add(sub1);
