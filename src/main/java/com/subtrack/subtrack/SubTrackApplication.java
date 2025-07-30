4445695dcc7fc5454b9609c9118cb00e3cdef45d
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-07-30 22:26:18 +0530

    DataSourceAutoConfiguration added in SubTrackApplication.java class

--- src/main/java/com/subtrack/subtrack/SubTrackApplication.java
+++ src/main/java/com/subtrack/subtrack/SubTrackApplication.java
@@ -2,8 +2,9 @@ package com.subtrack.subtrack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SubTrackApplication {
 
 	public static void main(String[] args) {
