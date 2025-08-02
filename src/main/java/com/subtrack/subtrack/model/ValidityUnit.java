8a3239962666a9dd0e80d22f9c7b77986e23bf84
Samrat Saha <140338992+Samsgithub9635@users.noreply.github.com>
2025-08-02 21:06:57 +0530

    feat: Add displayName property to ValidityUnit enum

--- src/main/java/com/subtrack/subtrack/model/ValidityUnit.java
+++ src/main/java/com/subtrack/subtrack/model/ValidityUnit.java
@@ -1,7 +1,17 @@
 package com.subtrack.subtrack.model;
 
 public enum ValidityUnit {
-    DAYS,
-    MONTHS,
-    YEARS
+    DAYS("Days"),
+    MONTHS("Months"),
+    YEARS("Years");
+
+    private final String displayName;
+
+    ValidityUnit(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
